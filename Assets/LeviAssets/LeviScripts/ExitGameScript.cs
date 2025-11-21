880f46011a4c2716c20739b58bf0aed493fdac33
todorolo <159575710+todorolo@users.noreply.github.com>
2025-11-21 12:13:55 -0500

    class 11/21
    class stuff idk

---
+++ Assets/LeviAssets/LeviScripts/ExitGameScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quitscript : MonoBehaviour
+{
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Game is exiting");
+    }
+}
